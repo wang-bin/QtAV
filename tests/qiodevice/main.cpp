9cee6fa9f91ac216e7091eab5546974a06c8bbf5
wang-bin <wbsecg1@gmail.com>
2014-02-02 22:50:55 +0800

    test: add playing from memory example

--- tests/qiodevice/main.cpp
+++ tests/qiodevice/main.cpp
@@ -3,6 +3,7 @@
 #include <QtCore/QTimer>
 #include <QtAV/QtAV.h>
 #include <QFile>
+#include <QBuffer>
 
 using namespace QtAV;
 
@@ -11,16 +12,28 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QFile vidfile(a.arguments().last());
-    AVPlayer player;
-    WidgetRenderer renderer;
 
-    if (vidfile.open(QIODevice::ReadOnly))
-    {
-        renderer.show();
-        player.addVideoRenderer(&renderer);
-        player.setIODevice(&vidfile);
-        player.play();
+    if (!vidfile.open(QIODevice::ReadOnly))
+        return 1;
+
+    AVPlayer player[2];
+    WidgetRenderer renderer[2];
+    renderer[0].show();
+    renderer[0].widget()->setWindowTitle("Test QFile");
+    renderer[1].show();
+    renderer[1].widget()->setWindowTitle("Test QBuffer. Play <=1M video from memory");
+    player[0].addVideoRenderer(&renderer[0]);
+    player[1].addVideoRenderer(&renderer[1]);
+
+    QByteArray data = vidfile.read(1024*1024);
+    vidfile.seek(0);
+    QBuffer buf(&data);
+    if (buf.open(QIODevice::ReadOnly)) {
+        player[1].setIODevice(&buf);
     }
+    player[0].setIODevice(&vidfile);
+    player[0].play();
+    player[1].play();
 
     return a.exec();
 }
--- tests/tests.pro
+++ tests/tests.pro
@@ -1,4 +1,5 @@
 TEMPLATE = subdirs
 
 SUBDIRS += \
+    qiodevice \
     playerthread
