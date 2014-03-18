46db3a0722c3111b533177b2a882f4c234068884
wang-bin <wbsecg1@gmail.com>
2014-03-18 12:45:29 +0800

    build error and typo

--- src/AudioResamplerFF.cpp
+++ src/AudioResamplerFF.cpp
@@ -1,8 +1,29 @@
+/******************************************************************************
+    QtAV:  Media play library based on Qt and FFmpeg
+    Copyright (C) 2012-2014 Wang Bin <wbsecg1@gmail.com>
 
-#include "AudioResampler.h"
-#include "private/AudioResampler_p.h"
+*   This file is part of QtAV
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+******************************************************************************/
+
+
+#include "QtAV/AudioResampler.h"
+#include "QtAV/private/AudioResampler_p.h"
 #include "QtAV/QtAV_Compat.h"
-#include "prepost.h"
+#include "QtAV/prepost.h"
 
 namespace QtAV {
 
@@ -156,7 +177,7 @@ bool AudioResamplerFF::prepare()
     */
     qDebug("out: {cl: %lld, fmt: %s, freq: %d}"
            , d.out_format.channelLayoutFFmpeg()
-           , qPrintable(d.out_format.sampleFormat())
+           , qPrintable(d.out_format.sampleFormatName())
            , d.out_format.sampleRate());
     qDebug("in {cl: %lld, fmt: %s, freq: %d}"
            , d.in_format.channelLayoutFFmpeg()
