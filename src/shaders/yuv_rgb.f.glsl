6b6f8bb08dd667f3e133819c31240adf285bf9de
wang-bin <wbsecg1@gmail.com>
2014-04-19 00:17:09 +0800

    gl: add shader license

--- src/shaders/yuv_rgb.f.glsl
+++ src/shaders/yuv_rgb.f.glsl
@@ -1,3 +1,24 @@
+/******************************************************************************
+    QtAV:  Media play library based on Qt and FFmpeg
+    Copyright (C) 2012-2014 Wang Bin <wbsecg1@gmail.com>
+
+*   This file is part of QtAV
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+******************************************************************************/
+
 #ifdef GL_ES
 // Set default precision to medium
 precision mediump int;
@@ -57,6 +78,7 @@ void main()
                              (texture2D(u_Texture1, v_TexCoords).r*256.0 + texture2D(u_Texture1, v_TexCoords).a)*255.0/range,
                              (texture2D(u_Texture2, v_TexCoords).r*256.0 + texture2D(u_Texture2, v_TexCoords).a)*255.0/range,
 #else
+// use r, g, a to work for both yv12 and nv12. idea from xbmc
                              texture2D(u_Texture0, v_TexCoords).r,
                              texture2D(u_Texture1, v_TexCoords).g,
                              texture2D(u_Texture2, v_TexCoords).a,
