4da373da073652172b7f91c6595a17e186445c7f
wang-bin <wbsecg1@gmail.com>
2014-11-04 13:59:05 +0800

    hue, saturation transform in yuv

--- src/ColorTransform.cpp
+++ src/ColorTransform.cpp
@@ -22,13 +22,33 @@
 #include "QtAV/ColorTransform.h"
 #include <QtCore/qmath.h>
 
+#define RGB_INTERMEDIATE 0
 namespace QtAV {
 
 static const QMatrix4x4 kGBR2RGB = QMatrix4x4(0, 0, 1, 0,
                                               1, 0, 0, 0,
                                               0, 1, 0, 0,
                                               0, 0, 0, 1);
-
+#if 1
+// from mpv
+static QMatrix4x4 luma_coeffs(qreal lr, qreal lg, qreal lb)
+{
+    Q_ASSERT(qFuzzyCompare(lr + lg + lb, 1.0));
+    QMatrix4x4 m;
+    m(0, 0) = m(1, 0) = m(2, 0) = 1;
+    m(1, 1) = -2*(1-lb)*lb/lg;
+    m(2, 1) = 2*(1-lb);
+    m(0, 2) = 2*(1-lr);
+    m(1, 2) = -2*(1-lr)*lr/lg;
+    m(2, 2) = 0;
+    // YUV [0,1]x[-0.5,0.5]x[-0.5,0.5] -> [0,1]x[0,1]x[0,1]
+    return m * QMatrix4x4(
+                1.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f, -0.5f,
+                0.0f, 0.0f, 1.0f, -0.5f,
+                0.0f, 0.0f, 0.0f, 1.0f);
+}
+#else
 static const QMatrix4x4 yuv2rgb_bt601 =
            QMatrix4x4(
                 1.0f,  0.000f,  1.402f, 0.0f,
@@ -54,9 +74,11 @@ static const QMatrix4x4 yuv2rgb_bt709 =
                 0.0f, 1.0f, 0.0f, -0.5f,
                 0.0f, 0.0f, 1.0f, -0.5f,
                 0.0f, 0.0f, 0.0f, 1.0f);
-
+#endif
 const QMatrix4x4& ColorTransform::YUV2RGB(ColorSpace cs)
 {
+    static const QMatrix4x4 yuv2rgb_bt601 = luma_coeffs(0.299,  0.587,  0.114);
+    static const QMatrix4x4 yuv2rgb_bt709 = luma_coeffs(0.2126, 0.7152, 0.0722);
     switch (cs) {
     case BT601:
         return yuv2rgb_bt601;
@@ -102,69 +124,96 @@ public:
         M.setToIdentity();
     }
     // TODO: optimize for other color spaces
+    // TODO: transform to output color space other than RGB
+    // ??? => RGB
     void compute() const {
         recompute = false;
+        const float b = brightness;
+        const float c = contrast + 1.0;
+        const float s = saturation + 1.0f;
+        const float h = hue * M_PI;               // hue rotation angle
         //http://docs.rainmeter.net/tips/colormatrix-guide
         //http://www.graficaobscura.com/matrix/index.html
         //http://beesbuzz.biz/code/hsv_color_transforms.php
         // ??
-        float b = brightness;
-        // brightness R,G,B
-        QMatrix4x4 B(1, 0, 0, b,
-                     0, 1, 0, b,
-                     0, 0, 1, b,
-                     0, 0, 0, 1);
-        float c = contrast+1.0;
+        // brightness translate R,G,B by a const
+        QMatrix4x4 B;
+        B.translate(b, b, b);
+#ifndef CONTRAST_YUV
         // Contrast (offset) R,G,B
-        QMatrix4x4 C(c, 0, 0, 0,
-                     0, c, 0, 0,
-                     0, 0, c, 0,
-                     0, 0, 0, 1);
-        // Saturation
-        const float wr = 0.3086f;
-        const float wg = 0.6094f;
-        const float wb = 0.0820f;
-        float s = saturation + 1.0f;
-        QMatrix4x4 S(
-            (1.0f - s)*wr + s, (1.0f - s)*wg    , (1.0f - s)*wb    , 0.0f,
-            (1.0f - s)*wr    , (1.0f - s)*wg + s, (1.0f - s)*wb    , 0.0f,
-            (1.0f - s)*wr    , (1.0f - s)*wg    , (1.0f - s)*wb + s, 0.0f,
-                         0.0f,              0.0f,              0.0f, 1.0f
-        );
-        // Hue
-        const float n = 1.0f / sqrtf(3.0f);       // normalized hue rotation axis: sqrt(3)*(1 1 1)
-        const float h = hue*M_PI;               // hue rotation angle
-        const float hc = cosf(h);
-        const float hs = sinf(h);
-        QMatrix4x4 H(     // conversion of angle/axis representation to matrix representation
-            n*n*(1.0f - hc) + hc  , n*n*(1.0f - hc) - n*hs, n*n*(1.0f - hc) + n*hs, 0.0f,
-            n*n*(1.0f - hc) + n*hs, n*n*(1.0f - hc) + hc  , n*n*(1.0f - hc) - n*hs, 0.0f,
-            n*n*(1.0f - hc) - n*hs, n*n*(1.0f - hc) + n*hs, n*n*(1.0f - hc) + hc  , 0.0f,
-                              0.0f,                   0.0f,                   0.0f, 1.0f
-        );
+        QMatrix4x4 C;
+        C.scale(c, c, c);
+#endif
+        if (RGB_INTERMEDIATE || in == ColorTransform::RGB || in == ColorTransform::GBR) {
+            // Saturation
+            const float wr = 0.3086f;
+            const float wg = 0.6094f;
+            const float wb = 0.0820f;
+            const QMatrix4x4 S(
+                (1.0f - s)*wr + s, (1.0f - s)*wg    , (1.0f - s)*wb    , 0.0f,
+                (1.0f - s)*wr    , (1.0f - s)*wg + s, (1.0f - s)*wb    , 0.0f,
+                (1.0f - s)*wr    , (1.0f - s)*wg    , (1.0f - s)*wb + s, 0.0f,
+                             0.0f,              0.0f,              0.0f, 1.0f
+            );
+            // Hue
+            const float n = 1.0f / sqrtf(3.0f);       // normalized hue rotation axis: sqrt(3)*(1 1 1)
+            const float hc = cosf(h);
+            const float hs = sinf(h);
+            const QMatrix4x4 H(     // conversion of angle/axis representation to matrix representation
+                n*n*(1.0f - hc) + hc  , n*n*(1.0f - hc) - n*hs, n*n*(1.0f - hc) + n*hs, 0.0f,
+                n*n*(1.0f - hc) + n*hs, n*n*(1.0f - hc) + hc  , n*n*(1.0f - hc) - n*hs, 0.0f,
+                n*n*(1.0f - hc) - n*hs, n*n*(1.0f - hc) + n*hs, n*n*(1.0f - hc) + hc  , 0.0f,
+                                  0.0f,                   0.0f,                   0.0f, 1.0f
+            );
 
-        M = B*C*S*H;
-        // TODO: transform to output color space other than RGB
-        switch (in) {
-        case ColorTransform::RGB:
-            break;
-        case ColorTransform::GBR:
-            M *= kGBR2RGB;
-            break;
-        default:
-            M *= YUV2RGB(in);
-            break;
+            M = B*C*S*H;
+            if (in == ColorTransform::GBR) {
+                M *= kGBR2RGB;
+            }
+#if RGB_INTERMEDIATE
+            else if (in != ColorTransform::RGB) {
+                M *= YUV2RGB(in);
+            }
+#endif
+            switch (out) {
+            case ColorTransform::RGB:
+                break;
+            case ColorTransform::GBR:
+                M = kGBR2RGB.inverted() * M;
+                break;
+            default: //yuv
+                M = YUV2RGB(out).inverted() * M;
+                break;
+            }
+        } else { // YUV
+            QMatrix4x4 H, S;
+            // rotate UV
+            H.rotate(h/M_PI*180.0, 1, 0, 0); //degree
+            // scale U, V
+            S.scale(1, s, s);
+            // TODO: contrast
+#ifdef CONTRAST_YUV
+            // scale Y around 1/2
+            //C.scale(c, 1, 1);
+            //B.translate(0.5*(1.0-c), 0.5*(1.0-c), 0.5*(1.0-c));
+            //M = B * YUV2RGB(in) * H * S * C;
+#else
+            //M = B * C * YUV2RGB(in) * H * S;
+#endif
+            switch (out) {
+            case ColorTransform::RGB:
+                M = B * C * YUV2RGB(in) * H * S;
+                break;
+            case ColorTransform::GBR:
+                M = B * C * YUV2RGB(in) * H * S;
+                M = kGBR2RGB.inverted() * M;
+                break;
+            default:
+                M = YUV2RGB(out).inverted() * B * C * YUV2RGB(in) * H * S; // TODO: no yuv->rgb
+                break;
+            }
         }
-        switch (out) {
-        case ColorTransform::RGB:
-            break;
-        case ColorTransform::GBR:
-            M = kGBR2RGB.inverted() * M;
-            break;
-        default:
-            M = YUV2RGB(out).inverted() * M;
-            break;
-        }
+        //qDebug() << YUV2RGB(in) << M;
     }
 
     mutable bool recompute;
