d356ecbaf666db4f3976095f106e73bb39e5c4a8
Thiago A. Correa <thiago.correa@gmail.com>
2015-02-18 14:16:47 -0200

    Fix shader loading when the library is built statically
    Signed-off-by: Thiago A. Correa <thiago.correa@gmail.com>

--- src/QtAV_Global.cpp
+++ src/QtAV_Global.cpp
@@ -327,4 +327,18 @@ public:
 };
 InitFFmpegLog fflog;
 }
+    
+// Initialize Qt Resource System when the library is built
+// statically
+namespace {
+    static void initResources() {
+        Q_INIT_RESOURCE(shaders);
+        Q_INIT_RESOURCE(QtAV);
+    }
+    class ResourceLoader {
+        ResourceLoader() { initResources(); }
+    };
+    
+    ResourceLoader QtAV_QRCLoader;
+}
 }
