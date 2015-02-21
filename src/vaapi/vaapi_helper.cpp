aabc0ab4bfb78c22a36fc02d0de33bfb0d542a93
wang-bin <wbsecg1@gmail.com>
2015-02-21 20:18:16 +0800

    vaapi: fix va so load fail if libav-dev is not installed
    load so with version which is available in non-dev packages

--- src/vaapi/vaapi_helper.cpp
+++ src/vaapi/vaapi_helper.cpp
@@ -4,12 +4,21 @@
 namespace QtAV {
 namespace vaapi {
 
-dll_helper::dll_helper(const QString &soname)
+dll_helper::dll_helper(const QString &soname, int version)
 {
-    m_lib.setFileName(soname);
-    if (m_lib.load())
-        qDebug("%s loaded", m_lib.fileName().toUtf8().constData());
+    if (version >= 0)
+        m_lib.setFileNameAndVersion(soname, version);
     else
+        m_lib.setFileName(soname);
+    if (m_lib.load()) {
+        qDebug("%s loaded", m_lib.fileName().toUtf8().constData());
+    } else {
+        if (version >= 0) {
+            m_lib.setFileName(soname);
+            m_lib.load();
+        }
+    }
+    if (!m_lib.isLoaded())
         qDebug("can not load %s: %s", m_lib.fileName().toUtf8().constData(), m_lib.errorString().toUtf8().constData());
 }
 
