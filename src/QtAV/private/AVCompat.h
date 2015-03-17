493d31628b730bc96fb197950ad9279a7bb47614
Marco Trevisan (Treviño) <mail@3v1n0.net>
2015-03-17 19:49:58 +0100

    AVCompat: include libavutil/channel_layout.h only on new versions

--- src/QtAV/private/AVCompat.h
+++ src/QtAV/private/AVCompat.h
@@ -47,12 +47,15 @@ extern "C"
 #include <libavutil/dict.h>
 #include <libavutil/log.h>
 #include <libavutil/mathematics.h> //AV_ROUND_UP, av_rescale_rnd for libav
-#include <libavutil/channel_layout.h>
 #include <libavutil/cpu.h>
 #include <libavutil/error.h>
 #include <libavutil/opt.h>
 #include <libavutil/parseutils.h>
 #include <libavutil/pixdesc.h>
+
+#if !FFMPEG_MODULE_CHECK(LIBAVUTIL, 51, 73, 101)
+#include <libavutil/channel_layout.h>
+#endif
 
 /* TODO: how to check whether we have swresample or not? how to check avresample?*/
 #include <libavutil/samplefmt.h>
