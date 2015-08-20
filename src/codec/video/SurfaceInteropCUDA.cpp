7ebecc1fdc7ecd9ade864b54b27d0c42180438f6
wang-bin <wbsecg1@gmail.com>
2015-08-20 22:37:33 +0800

    cuda: fix build error

--- src/codec/video/SurfaceInteropCUDA.cpp
+++ src/codec/video/SurfaceInteropCUDA.cpp
@@ -29,9 +29,6 @@
 
 namespace QtAV {
 namespace cuda {
-#define MS_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
-    static const GUID name = { l, w1, w2, {b1, b2, b3, b4, b5, b6, b7, b8}}
-MS_GUID(IID_IDirect3DTexture9,            0x85c31227, 0x3de5, 0x4f00, 0x9b, 0x3a, 0xf1, 0x1a, 0xc3, 0x8c, 0x18, 0xb5);
 
 InteropResource::InteropResource(CUdevice d, CUvideodecoder decoder, CUvideoctxlock declock)
     : cuda_api()
@@ -186,7 +183,6 @@ EGLInteropResource::EGLInteropResource(CUdevice d, CUvideodecoder decoder, CUvid
     , query9(NULL)
 {
 }
-
 
 EGLInteropResource::~EGLInteropResource()
 {
