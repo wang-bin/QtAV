3e4a8dad913374f0d6519ddfe6c395648c820bff
wang-bin <wbsecg1@gmail.com>
2015-10-26 23:43:42 +0800

    coda vpu: add auto lock for gco surface

--- src/codec/video/viv/GALScaler.h
+++ src/codec/video/viv/GALScaler.h
@@ -8,6 +8,7 @@ public:
     GALScaler();
     bool check() const Q_DECL_OVERRIDE;
     // FIXME: why match to the pure virtual one if not declare here?
+    // TODO: move to private
     bool convert(const quint8 *const src[], const int srcStride[]) Q_DECL_OVERRIDE;
     // dst is allocated by gpu. if convert to given dst(host), use dma copy
     bool convert(const quint8 *const src[], const int srcStride[], quint8 *const dst[], const int dstStride[]) Q_DECL_OVERRIDE;
