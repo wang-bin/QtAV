86a7dc87e7dcb5773ab4be484e36202364f73858
wang-bin <wbsecg1@gmail.com>
2015-12-08 17:52:45 +0800

    ci: use trusty for travis. add gcc

--- tools/ci/linux/script.sh
+++ tools/ci/linux/script.sh
@@ -9,13 +9,13 @@ cat /etc/issue
 echo "QtAV build script for travis-ci"
 
 jobs=`cat /proc/cpuinfo |grep 'cpu cores' |wc -l`
-
-
 cd $QTAV_OUT
 
 rm -f build.log
 type -a moc
-qmake -r $TRAVIS_BUILD_DIR -spec linux-clang "CONFIG+=recheck"
-make -j$jobs
+SPEC=linux-clang
+test -n "$CC" && test "${CC/gcc/}" != "$CC" && SPEC=linux-g++
+qmake -r $TRAVIS_BUILD_DIR -spec $SPEC "CONFIG+=recheck"
+make -j$((jobs+1))
 
 cd $TRAVIS_BUILD_DIR
