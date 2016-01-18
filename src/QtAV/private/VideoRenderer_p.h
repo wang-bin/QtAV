933b082d8e7f15055d8e3fefd7ec6aa1223b2113
wang-bin <wbsecg1@gmail.com>
2016-01-18 12:41:54 +0800

    vo: renderer size rounding #570

--- src/QtAV/private/VideoRenderer_p.h
+++ src/QtAV/private/VideoRenderer_p.h
@@ -82,12 +82,12 @@ public:
         if (rendererAspectRatio >= dar) { //equals to original video aspect ratio here, also equals to out ratio
             //renderer is too wide, use renderer's height, horizonal align center
             const int h = renderer_height;
-            const int w = dar * qreal(h);
+            const int w = qRound(dar * qreal(h));
             out_rect = QRect((renderer_width - w)/2, 0, w, h);
         } else if (rendererAspectRatio < dar) {
             //renderer is too high, use renderer's width
             const int w = renderer_width;
-            const int h = qreal(w)/dar;
+            const int h = qRound(qreal(w)/dar);
             out_rect = QRect(0, (renderer_height - h)/2, w, h);
         }
         out_aspect_ratio = outAspectRatio;
