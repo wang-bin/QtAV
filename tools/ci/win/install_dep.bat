9f62d3af6e3b9da06637e72d8463fdaf2322c535
wang-bin <wbsecg1@gmail.com>
2016-04-03 23:45:21 +0800

    appveyor: cache dependency
    TODO: md5 check before downloading?

--- tools/ci/win/install_dep.bat
+++ tools/ci/win/install_dep.bat
@@ -1,7 +1,10 @@
-echo downloading qtav dep "http://sourceforge.net/projects/qtav/files/depends/QtAV-depends-windows-x86+x64.7z/download"
-appveyor DownloadFile "http://sourceforge.net/projects/qtav/files/depends/QtAV-depends-windows-x86+x64.7z/download" -FileName av.7z
-7z x av.7z > NUL
-move QtAV-depends* AV
+:: TODO: check md5
+if not exist AV (
+  echo downloading qtav dep "http://sourceforge.net/projects/qtav/files/depends/QtAV-depends-windows-x86+x64.7z/download"
+  appveyor DownloadFile "http://sourceforge.net/projects/qtav/files/depends/QtAV-depends-windows-x86+x64.7z/download" -FileName av.7z
+  7z x av.7z > NUL
+  move QtAV-depends* AV
+)
 xcopy /syi AV\include %QTDIR%\include > NUL
 if "%arch%" == "x64" (
   xcopy /syi AV\lib\x64 %QTDIR%\lib > NUL
