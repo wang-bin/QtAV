34dd17288d41bfe90c7f252a7aceee702b9844b3
wang-bin <wbsecg1@gmail.com>
2016-10-23 13:20:08 +0800

    deb: fix ubuntu16.10 quick control dependency

--- debian/ubuntu.sh
+++ debian/ubuntu.sh
@@ -1,4 +1,5 @@
 CHANGELOG_BAK=/tmp/qtav_changelog
+CONTROL_BAK=/tmp/qtav_control
 mkchangelog() {
   cat >debian/changelog<<EOF
 qtav ($1) $2; urgency=low
@@ -13,12 +14,19 @@ EOF
 
 DISTRIBUTIONS=(trusty vivid wily xenial yakkety)
 DATE=`date -d @$(git log -n1 --format="%at") +%Y%m%d`
+idx=0
 for D in ${DISTRIBUTIONS[@]}; do
-  git checkout -- debian/changelog
+  git checkout -- debian/changelog debian/control
+  cp -avf debian/control $CONTROL_BAK
   cp -avf debian/changelog $CHANGELOG_BAK
   VER=1.11.0~`git log -1 --pretty=format:"git${DATE}.%h~${D}" 2> /dev/null`
   mkchangelog $VER $D
+  if [ $idx -gt 3 ]; then
+    sed -i 's,qtdeclarative5-controls-plugin,qml-module-qtquick-controls,g' debian/control
+  fi
   debuild -S -sa
   dput -f ppa:wbsecg1/qtav ../qtav_${VER}_source.changes
   cp -avf $CHANGELOG_BAK debian/changelog
+  cp -avf $CONTROL_BAK debian/control
+  idx=$((idx+1))
 done
