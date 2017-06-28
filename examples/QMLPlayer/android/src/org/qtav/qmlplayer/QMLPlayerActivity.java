0737e63ca2734764c624c34317d5392feaeac066
wang-bin <wbsecg1@gmail.com>
2017-06-28 10:52:12 +0800

    android: fix 6.0 permission

--- examples/QMLPlayer/android/src/org/qtav/qmlplayer/QMLPlayerActivity.java
+++ examples/QMLPlayer/android/src/org/qtav/qmlplayer/QMLPlayerActivity.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.app.PendingIntent;
 import android.util.Log;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 public class QMLPlayerActivity extends QtActivity
 {
@@ -23,6 +28,10 @@ public class QMLPlayerActivity extends QtActivity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!checkPermission())
+                requestPermission();
+        }
         //getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         Intent intent = getIntent();
@@ -34,5 +43,20 @@ public class QMLPlayerActivity extends QtActivity
             m_request_url = intent.getDataString();
         }
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    protected boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+        return false;
+    }
+    protected void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, "Please allow Write External Storage permission to play your local videos", Toast.LENGTH_LONG).show();
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
     }
 }
