e92757f9677d72117cba10d30db064cfadd7ef03
wang-bin <wbsecg1@gmail.com>
2017-07-19 16:51:03 +0800

    qml: fix user filter ptr

--- qml/QuickFilter.cpp
+++ qml/QuickFilter.cpp
@@ -1,3 +1,24 @@
+/******************************************************************************
+    QtAV:  Multimedia framework based on Qt and FFmpeg
+    Copyright (C) 2012-2017 Wang Bin <wbsecg1@gmail.com>
+
+*   This file is part of QtAV (from 2016)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+******************************************************************************/
+
 #include "QmlAV/QuickFilter.h"
 #include "QtAV/private/Filter_p.h"
 #include "QtAV/LibAVFilter.h"
@@ -85,6 +106,8 @@ void QuickVideoFilter::setUserFilter(VideoFilter *f)
     if (d.user_filter.data() == f)
         return;
     d.user_filter.reset(f);
+    if (d.type == UserFilter)
+        d.filter = d.user_filter.data();
     Q_EMIT userFilterChanged();
 }
 
@@ -177,6 +200,8 @@ void QuickAudioFilter::setUserFilter(AudioFilter *f)
     if (d.user_filter.data() == f)
         return;
     d.user_filter.reset(f);
+    if (d.type == UserFilter)
+        d.filter = d.user_filter.data();
     Q_EMIT userFilterChanged();
 }
 
