2266afb7b87b3e372214d66ca53f57aa32a1af25
afshar <nejatafshar@gmail.com>
2018-03-12 16:09:28 +0330

    + update ffmpeg windows libs to version 3.4.1

--- ffmpegBin/Win/32/include/libavutil/ffversion.h
+++ ffmpegBin/Win/32/include/libavutil/ffversion.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:102510d720d94a932c770f85a7e4f3b099a978b7649ddf21101d77558e8504ff
-size 187
+oid sha256:ac874d52bf9cbb1482a287b879bca1b396252f34e2525fb2608b685b6f0103eb
+size 189
