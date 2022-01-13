22c2909e578cfec1cd86770737dc6764aa5f478f
waterstrider <waterstrider.vin@gmail.com>
2022-01-13 14:36:40 +0700

    Update main.cpp
    add missing import to make it compile

--- examples/glslfilter/main.cpp
+++ examples/glslfilter/main.cpp
@@ -18,6 +18,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 #include <QtAV>
+#include <QtAV/VideoShaderObject.h>
 #include <QApplication>
 #include <QtCore/QScopedPointer>
 #include <QtCore/qmath.h>
